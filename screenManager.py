447a8f2366c179a9a0ff59077b34fd6a118c1dbb
ckaddouh <christinakaddouh@gmail.com>
2020-01-29 11:42:00 -0500

    CardObject Started!

--- screenManager.py
+++ screenManager.py
@@ -3,15 +3,20 @@
 from tkinter import *
 
 class Memory_Manager(object):
-    def __init__(self):
+    def __init__(self, numOfCards):
         # Initializes a new battle manager by loading the card images and by initializing tkinter.  
         self.root = tkinter.Tk()
         self.current_screen = None
+        self.numOfCards = numOfCards
         c = Canvas(self.root, height = 300, width = 350, bg = "blue")
         c.pack()
         
 
-    #def flipCard(self):
+    def displayCards(self):
+        cardDict = {}
+        
+        
+    def flipCard(self):
         # Uses Canvas to illustrate a card flipping over.  
 
 root.mainloop()
