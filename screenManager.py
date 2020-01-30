eca6fde322b78dd4ad44618ba68d58849b255028
Kaylee <60186448+kaynam23@users.noreply.github.com>
2020-01-30 11:07:33 -0500

    hi

--- screenManager.py
+++ screenManager.py
@@ -7,9 +7,6 @@ class Memory_Manager(object):
         # Initializes a new battle manager by loading the card images and by initializing tkinter.  
         self.root = tkinter.Tk()
         self.current_screen = None
-<<<<<<< HEAD
-
-=======
         self.numOfCards = numOfCards
         c = Canvas(self.root, height = 300, width = 350, bg = "blue")
         c.pack()
@@ -19,7 +16,6 @@ class Memory_Manager(object):
         cardDict = {}
         
         
->>>>>>> 447a8f2366c179a9a0ff59077b34fd6a118c1dbb
     def flipCard(self):
         # Uses Canvas to illustrate a card flipping over.  
 
