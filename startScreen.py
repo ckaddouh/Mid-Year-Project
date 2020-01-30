a9af684789e7fb8dc80080b352af722d214abaeb
Kaylee <60186448+kaynam23@users.noreply.github.com>
2020-01-30 11:39:21 -0500

    startScreen

--- startScreen.py
+++ startScreen.py
@@ -1,21 +1,27 @@
 from tkinter import *
 
 class Start_screen(Frame):
-    def __init__(self, master, call_on_selected):
+    def __init__(self, master):
         super(Start_screen, self).__init__(master)
 
-        self.call_on_selcted = call_on_selected
+        #self.call_on_selcted = call_on_selected
 
         self.grid()
 
-        self.createwidgets()
+        self.create_widgets()
 
     def create_widgets(self):
         
         Label(self, text = "Memory Card Game", font = "Helvetica 20 bold").grid(row = 0, column = 0, sticky = N)
-        Label(self, text = "Click on two cards. If they match, you get a point! If they don't, they'll flip back over. Try to remember the cards so you can match all the pairs and win!").grid(row = 1, column = 0, sticky = N)
-        startbutton= Button(self, text = "START GAME", font = "Courier 16 bold", command = self.continue_clicked)
-        startbutton.grid(row = 2, column = 0, sticky = N)
+        Label(self, text = "Click on two cards. If they match, you get a point!\n If they don't, they'll flip back over. \nTry to remember the cards so you can match all the pairs and win!").grid(row = 1, column = 0, sticky = N)
+        start_button= Button(self, text = "START GAME", font = "Courier 16 bold")
+        start_button.grid(row = 2, column = 0, sticky = N)
     
-    def continue_clicked(self):
-        self.call_on_selcted(self.game.get())
+    #def continue_clicked(self):
+        #self.call_on_selcted(self.game.get())
+
+root = Tk()
+root.title("StartScreen")
+root.geometry("410x150")
+app = Start_screen(root)
+root.mainloop()
