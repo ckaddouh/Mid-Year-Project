a1f58f5271b589eebdce788c88424de5d3c3895b
ckaddouh <christinakaddouh@gmail.com>
2020-01-30 11:41:57 -0500

    Working on it

--- CardObject.py
+++ CardObject.py
@@ -1,9 +1,11 @@
 # Card Object
 class Card():
-    def __init__(self, imageName):
+    def __init__(self, imageName, number, suit):
         self.imageName = imageName
         cardID = self.imageName[0: 2]
         self.cardID = cardID
+        self.number = number
+        self.suit = suit
     
     def flip(self, curShow, curHid):
     
--- displayCard.py
+++ displayCard.py
@@ -1,5 +1,18 @@
 # This file selects cards, randomizes them, and displays them to the screen.  
-
+from CardObject import Card
 import random
 
 cardDict = {}
+suitList = ["C", "D", "H", "S"]
+numberList = ("1 2 3 4 5 6 7 8 9 A J K Q").split()
+
+for i in range(12):
+
+    cSuit = random.choice(suitList)
+    cNumber = random.choice(numberList)
+    name = cSuit + cNumber + ".jpg"
+    if name not in list(cardDict.keys()):
+        card = Card(name, cSuit, cNumber)
+        cardDict[name] = card
+    else:
+        continue
