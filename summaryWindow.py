2781ba07702c0f5a8ffcfe9b9681016779b9e516
dongjoo224 <60186548+dongjoo224@users.noreply.github.com>
2020-01-31 11:28:14 -0500

    hi

--- summaryWindow.py
+++ summaryWindow.py
@@ -1,4 +1,26 @@
 from tkinter import *
 
 class summary(Frame):
-    def __init__()
+    def __init__(self, master):
+        super(summaryWindow, self).__init__(master)
+
+        self.call_on_selcted = call_on_selected
+
+        self.grid()
+        
+        self.create_widgets()
+
+    def create_widgets(self):
+        Label(self, text = "You Win!", font = "Helvetica 20 bold").grid(row = 0, column = 0, sticky = N)
+        Label(self, text = "It took you %s to match %s pairs." % (time, numPairs), font = "Helvetica 20 bold").grid(row = 1, column = 0, sticky = N)
+        Label(self).grid(row = 2,column = 0)
+
+        retry_button= Button(self, text = "Retry", font = "Courier 16 bold", command = self.game)
+        retry_button.grid(row = 3, column = 0, sticky = W)
+
+        exit_button= Button(self, text = "Exit", font = "Courier 16 bold", command = self.close_screen)
+        exit_button.grid(row = 3, column = 1, sticky = E)
+
+
+    def continue_clicked(self):
+        self.call_on_selcted(self.start_button.get())
