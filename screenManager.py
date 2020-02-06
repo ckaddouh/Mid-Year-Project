7dab08b2de3b9e5d6f2591a678a4ca7167a3f048
Annie <annie.li.29122@gmail.com>
2020-02-06 11:18:36 -0500

    commit # 121

--- screenManager.py
+++ screenManager.py
@@ -1,26 +1,27 @@
-# This file will manage the various screens of the game.  
+# This file will manage the various screens of the game.
 
 from tkinter import *
 from startScreen import Start_screen
 from displayCard import Application
 from summaryWindow import summary
 
-class Memory_Manager(object):
+
+class Memory_Manager (object):
     def __init__(self):
-        # Initializes a new battle manager by loading the card images and by initializing tkinter.  
+        # Initializes a new battle manager by loading the card images and by initializing tkinter.
         self.root = Tk()
         self.current_screen = None
-        # self.numOfCards = numOfCards
+        self.numOfCards = numOfCards
         self.numOfCards = 24
 
     def startscreen(self):
         self.root.title("Welcome! ")
-        self.current_screen = Start_screen(self.root)
+        self.current_screen = Start_screen(self.root, self.start_button)
 
     def start_button(self):
         self.current_screen.destroy()
         self.game()
-    
+
     def game(self):
         self.root.title("Memory!")
         self.current_screen = Application(self.root)
@@ -29,9 +30,10 @@ class Memory_Manager(object):
         self.current_screen.destroy()
         self.root.title("Game Summary")
         self.current_screen = summary(self.root)
-
+    
     def close_screen(self):
         self.current_screen.destroy()
+
 
 def main():
     memory = Memory_Manager()
