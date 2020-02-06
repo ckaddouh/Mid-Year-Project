1a87139e7f8390dd355bf41b7c8c3462384b81fb
ckaddouh <christinakaddouh@gmail.com>
2020-02-06 11:41:15 -0500

    Fixing memory.py

--- CardObject.py
+++ CardObject.py
@@ -1,11 +1,9 @@
 # Card Object
 class Card():
-    def __init__(self, imageName, number, suit):
+    def __init__(self, imageName):
         self.imageName = imageName
-        cardID = self.imageName[0: 2]
-        self.cardID = cardID
-        self.number = number
-        self.suit = suit
+        self.cardID = self.imageName[0: 2]
+
     
     def hide(self):
         curCardDict[self]["image"] = 
