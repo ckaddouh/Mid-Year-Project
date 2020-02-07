942653e85c78ac179d9b4c646d5eae5c84b9ecb4
Kaylee <60186448+kaynam23@users.noreply.github.com>
2020-02-07 11:17:40 -0500

    call_on_next fixed

--- screenManager.py
+++ screenManager.py
@@ -24,7 +24,7 @@ class Memory_Manager (object):
 
     def game(self):
         self.root.title("Memory!")
-        self.current_screen = Application(self.root)
+        self.current_screen = Application(self.root, self.summary)
     
     def summary(self):
         self.current_screen.destroy()
