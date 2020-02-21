8c081822cf61b80cb157438a590c45b2b28deea0
ckaddouh <ckaddouh@gmail.com>
2020-02-20 21:16:47 -0500

    Comments added

--- memory.py
+++ memory.py
@@ -1,72 +1,67 @@
-# This file contains the logistics of the memory game.
-# Christina suuuuucks
+# This file contains the logistics of the memory game and interacts with the card object.
+
 from tkinter import *
 from cardObject import Card
 import random
-import time
 
+# Define a class Application which creates a screen and sets initial values for dictionaries, lists, and images.
 class Application(Frame):
     def __init__(self, master, call_on_next):
         super(Application, self).__init__(master)
+        self.ImageDict = {}
+        self.ButtonDict = {}
         self.cardDict = {}
-        self.suitList = ("C D H S").split()
-        self.numberList = ("1 2 3 4 5 6 7 8 9 A J Q K").split()
         self.cardUpList = []
         self.cardGone = PhotoImage(file= "deck_of_cards/gray_block.png")
+        self.cardBack = PhotoImage(file= "deck_of_cards/purple_back.png")
         self.cardsGoneList = []
+
+        # Sets call_on_selected to a function which will pull up the next screen on the command of a button and formats the UI.
         self.call_on_selected = call_on_next
         self.selectCards()
         self.grid()
 
+    # A function which selects 12 random cards and duplicates them.
     def selectCards(self):
         allCardsList = []
-        self.ImageDict = {}
-        self.ButtonDict = {}
-        for i in self.suitList:
-            for x in self.numberList:
+
+        for i in ("C D H S").split():
+            for x in ("1 2 3 4 5 6 7 8 9 A J Q K").split():
                 allCardsList.append(x+i)
         for i in range(12):
             card = random.choice(allCardsList)
-            self.cardDict[card + "."] = Card("deck_of_cards/" + card + ".png", self.ImageDict, self.ButtonDict, self.cardDict, self.cardUpList, False, self.cardsGoneList)
-            self.cardDict[card + "x"] = Card("cards_copy/" + card + "x.png", self.ImageDict, self.ButtonDict, self.cardDict, self.cardUpList, False, self.cardsGoneList)
+            # These are placed in a dictionary which a key of their cardID and a value of a Card object.
+            # The copy has an additional "x" added on to its cardID.
+            self.cardDict[card + "."] = Card("deck_of_cards/" + card + ".png", self.ImageDict, self.ButtonDict, self.cardDict, self.cardUpList, False)
+            self.cardDict[card + "x"] = Card("cards_copy/" + card + "x.png", self.ImageDict, self.ButtonDict, self.cardDict, self.cardUpList, False)
             allCardsList.remove(card)
-
+        # Once the dictionary is randomly created, run createWidgets().
         self.createWidgets()
 
+    # A function which adds labels and buttons to the screen, which specifically creates a 4 x 6 grid of card objects.
     def createWidgets(self):
+        # Create a title at the top of the screen and randomize the keys of the card dictionary.
         Label(self, text = "MEMORY", font="Helvetica 20 bold").grid(row = 0, column = 3, columnspan = 2)
-        # self.checkButton.grid(row = 0, column = 6)
         self.cardIDList = list(self.cardDict.keys())
         random.shuffle(self.cardIDList)
         row = 1
         column = 1
-        self.cardBack = PhotoImage(file= "deck_of_cards/purple_back.png")
+        # Run through every key of the dictionary and load its image based on attributes of its object.
         for cardID in self.cardIDList:
             image1 = PhotoImage(file=self.cardDict[cardID].imageName)
+            # Add this image to an image dictionary which has a key of cardID and a value of the images.
             self.ImageDict[cardID] = image1
+            # Add this button to a button dictionary, and set its command to show when clicked.
             self.ButtonDict[cardID] = Button(self, text="", image=self.cardBack, command= "")
             self.ButtonDict[cardID].grid(row=row, column=column)
             self.ButtonDict[cardID]["command"] = self.cardDict[cardID].show
+            # Format the cards in a 4 x 6 grid.
             if column % 6 == 0 and column != 0:
                 row += 1
             column = column % 6 + 1
-        self.runGame()
+        # Create a button which calls the next screen.
+        Button(self, text="Game Over", command=self.continue_clicked).grid(row=5, column=5)
 
-    def allCommandShow(self):
-        for i in list(self.ButtonDict.keys()):
-            self.ButtonDict[i]["command"] = self.cardDict[i].show
-
-    def allCommandHide(self):
-        for i in list(self.ButtonDict.keys()):
-            self.ButtonDict[i]["command"] = self.cardDict[i].hide
-
-    def allCommandRemove(self):
-        for i in list(self.ButtonDict.keys()):
-            self.ButtonDict[i]["command"] = ""
-
-    def runGame(self):
-        self.endGameButton = Button(self, text="Game Over", command=self.continue_clicked)
-        self.endGameButton.grid(row=5, column=5)
-
+    # Call the next screen as defined in screenManager.py.
     def continue_clicked(self):
         self.call_on_selected()
--- summaryWindow.py
+++ summaryWindow.py
@@ -1,16 +1,24 @@
-# This window displays a summary screen.
+# This window displays a summary/end game screen.
 
 from tkinter import *
-from startScreen import Start_screen
+
+# Define a class summary which displays a game over message.
 class summary(Frame):
-    def __init__(self, master):
+    # Initialize tkinter, run create_widgets(), and format the screen with grid().
+    def __init__(self, master, exitScreen):
         super(summary, self).__init__(master)
+        self.call_on_selected = exitScreen
         self.create_widgets()
         self.grid()
 
+    # Create a congratulations and exit label on the screen.
     def create_widgets(self):
-            Label(self, text = "CONGRATULATIONS!", font="Helvetica 20 bold", fg = "white", bg = "purple").grid(row = 1, column = 1)
-            Label(self, text = "Come back to play soon!", font="Helvetica 20 bold").grid(row = 2, column = 1)
+        Label(self, text = "CONGRATULATIONS!", font="Helvetica 20 bold", fg = "white", bg = "purple").grid(row = 1, column = 1)
+        Label(self, text = "Come back to play soon!", font="Helvetica 20 bold").grid(row = 2, column = 1)
 
-            # Label(self, text="NICE TRY!", font="Helvetica 20 bold").grid(row=1, column=1)
-            # Label(self, text="Try and !", font="Helvetica 20 bold").grid(row=2, column=1)
+        # Create an exit button to exit the program.
+        Button(self, text="EXIT!", command=self.exit_clicked, fg="white", bg="red").grid(row=6, column=1, sticky=E)
+
+    # Define the exit to call the function in screenManager.py which closes the program.
+    def exit_clicked(self):
+        self.call_on_selected()
